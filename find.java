0ce2872fbec84baf35d581265809d4b7def90f19
Kalaiyarasan.A <kalaiyarasankalai9894@gmail.com>
2025-06-26 22:40:41 +0530

    JJ

---
+++ find.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class find{
+    void evenorodd(int num){
+ if (num%2==0) {
+    System.out.println("even number");
+     
+ }
+ else{
+    System.out.println("odd number");
+ }
+
+
+    }
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number=scan.nextInt();
+        find obj1=new find();
+        obj1.evenorodd(number);
+
+    }
+}
